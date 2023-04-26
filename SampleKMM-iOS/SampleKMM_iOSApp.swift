b35666a3aa0098332f9c7296e76c8d243d77ec81
wataru-taniuchi <wataru.taniuchi@asia-quest.jp>
2023-04-26 18:26:21 +0900

    Initial Commit

---
+++ SampleKMM-iOS/SampleKMM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleKMM_iOSApp.swift
+//  SampleKMM-iOS
+//
+//  Created by Taniuchi Wataru on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleKMM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
